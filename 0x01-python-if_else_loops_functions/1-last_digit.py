7f1a0ce326e043e15c7d68932222e93fe2481e8a
PeterEkwere <peterekwere04@gmail.com>
2023-06-06 06:17:29 -0700

    fixed hanling negative random numbers for task 1

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,7 +3,7 @@ import random
 number = random.randint(-10000, 10000)
 last = str(number)
 last = int(last[-1:])
-if number == (number * -1):
+if number < 0:
     last = last * -1
 if last > 5:
     print(f"Last digit of {number} is {last} and is greater than 5")
