6ee30e6897df0fcee276c9edc60142238065575e
PeterEkwere <peterekwere04@gmail.com>
2023-06-08 06:23:58 -0700

    updated 2-args.py with if block

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,16 +1,22 @@
 #!/usr/bin/python3
 
-import sys
+if __name__ == "__main__":
+    import sys
 
 i = 0
+j = 1
+
 while i < len(sys.argv) - 1:
     i += 1
 
+
 if len(sys.argv) == 1:
     print(f"0 arguments.")
+elif len(sys.argv) == 2:
+    print(f"{1} argument:")
+    print(f"{1}: {sys.argv[j]}")
 else:
-    j = 1
-    print(f"{i} argments:")
+    print(f"{i} arguments:")
     while j < len(sys.argv):
         print(f"{j}: {sys.argv[j]}")
         j += 1
