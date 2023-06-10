01e74df2ac60952b895334dacc2c9108ada5a289
PeterEkwere <peterekwere04@gmail.com>
2023-06-10 01:24:13 -0700

    fix2 requirement error for task 0

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
-if __name__ == "__main__":
-    print("This is a script")
-
 
 def print_list_integer(my_list=[]):
     i = 0
     length = len(my_list)
     for i in range(0, length):
-        print("{}".format(my_list[i]))
+        print("{:d}".format(my_list[i]))
