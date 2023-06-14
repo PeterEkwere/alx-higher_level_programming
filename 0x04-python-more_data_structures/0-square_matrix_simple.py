487f32438cdd5fd9a6ebdbde1a5bc4ab17d52ce4
PeterEkwere <peterekwere04@gmail.com>
2023-06-14 10:34:27 -0700

    created function for *.py script

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 
 def square_matrix_simple(matrix=[]):
-	for i in matrix:
+    if len(matrix) == 0:
+        return matrix
+    new_matrix = []
+    for row in matrix:
+        new_row = []
+        for number in row:
+            new_row.append(number ** 2)
+        new_matrix.append(new_row)
+    return new_matrix
