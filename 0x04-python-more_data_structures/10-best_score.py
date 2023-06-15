79db2e27eaeb9efde35a5507150f1af393880629
PeterEkwere <peterekwere04@gmail.com>
2023-06-14 17:38:58 -0700

    created a .py script for task 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+def best_score(a_dictionary):
+    name = ""
+    score = 0
+
+    if a_dictionary is None or len(a_dictionary) == 0:
+        return None
+
+    for k, v in a_dictionary.items():
+        if v > score:
+            score = v
+            name = k
+    return name
