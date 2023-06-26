2640717ff2efb62243f67a9273b04408a6895b9b
PeterEkwere <peterekwere04@gmail.com>
2023-06-26 09:42:44 -0700

    created a try/excepting script for task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    #this is for the main check
+    print()
+
+def safe_print_list(my_list=[], x=0):
+    try:
+        i = 0
+        length = 0
+        j = 0
+        for j in my_list:
+            length += 1
+        
+        while (i < x and i < length):
+            print(f"{my_list[i]}",end="")
+            i += 1
+        print("")
+        return i
+    except IndexError:
+        pass
