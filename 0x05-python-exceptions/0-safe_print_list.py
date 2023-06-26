d42bc48767cde8def0d9c95e9d7a3028326a7c3c
PeterEkwere <peterekwere04@gmail.com>
2023-06-26 09:45:23 -0700

    fixed coding style for project file

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    #this is for the main check
+    # this is for the main check
     print()
+
 
 def safe_print_list(my_list=[], x=0):
     try:
@@ -10,9 +11,9 @@ def safe_print_list(my_list=[], x=0):
         j = 0
         for j in my_list:
             length += 1
-        
+
         while (i < x and i < length):
-            print(f"{my_list[i]}",end="")
+            print(f"{my_list[i]}", end="")
             i += 1
         print("")
         return i
