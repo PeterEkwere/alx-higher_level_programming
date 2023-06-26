9687e24b2a245fd0593f07fef196d8660de41864
PeterEkwere <peterekwere04@gmail.com>
2023-06-26 10:03:23 -0700

    created .py script for task 1

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    # This is main
+    print()
+
+
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError:
+        return False
