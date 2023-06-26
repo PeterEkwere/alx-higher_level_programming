66ee8c365027c40432e394dc27fe2ced5828fd41
PeterEkwere <peterekwere04@gmail.com>
2023-06-26 16:12:20 -0700

    added TypeError catch to loop

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,19 +6,23 @@ if __name__ == "__main__":
 
 def list_division(my_list_1, my_list_2, list_length):
     new_list = []
+    result = 0
     for i in range(list_length):
         try:
             if (isinstance(my_list_1[i], (int, float)) and
                     isinstance(my_list_2[i], (int, float))):
-                new_list.append(my_list_1[i] / my_list_2[i])
+                result = my_list_1[i] / my_list_2[i]
             else:
                 print("wrong type")
-                new_list.append(0)
+        except TypeError:
+            print("wrong type")
+            result = 0
         except ZeroDivisionError:
             print("division by 0")
-            new_list.append(0)
-            continue
+            result = 0
         except IndexError:
             print("out of range")
-            continue
+            result = 0
+        finally:
+            new_list.append(result)
     return new_list
