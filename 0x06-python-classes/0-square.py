4d97b975daa856f43a8eddfa82153f7a1ae4987c
PeterEkwere <peterekwere04@gmail.com>
2023-06-27 14:15:10 -0700

    created .py script for task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """ This will execue if run directly """
+    print()
+""" This is a Module """
+
+
+class Square:
+    """ This is an Empty class """
+    def __init__(self):
+        """ Constructor for my Class """
+        pass
+
+# Running the command:
+# python3 -c 'print(__import__("0-square").Square.__doc__)'
