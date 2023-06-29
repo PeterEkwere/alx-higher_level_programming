47a8599466a4d715eea3bbbb162469795a21cda0
PeterEkwere <peterekwere04@gmail.com>
2023-06-29 02:26:19 -0700

    created .py script for task 5

---
+++ 0x05-python-exceptions/5-raise_exception.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+    This Module raises an exeption Type Error
+    Author: Ekwere Peter
+
+"""
+
+
+def raise_exception():
+    """ Raise a TypeError """
+    raise TypeError()
