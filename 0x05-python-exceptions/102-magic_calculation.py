5e937c2af1ef5bf3ee12c8ccacec42997d59420a
PeterEkwere <peterekwere04@gmail.com>
2023-06-29 10:14:58 -0700

    fixed pycodetyle for task 10

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -7,13 +7,10 @@
 
 def calculate_result(a, b):
     result = 0
-
-    for i in range(1, 4):
+    for i in range(1, 3):
         try:
-            if i < a:
-                raise Exception("Too far")
-            result += (a ** b) / i
-        except:
-            result += a + b
-
-    return result
+            result += a ** b / i
+        except Exception as e:
+            if str(e) != 'Too far':
+                raise
+    return result + b + a
