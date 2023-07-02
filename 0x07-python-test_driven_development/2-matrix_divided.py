7b80af82ef735147a313a237ed5382970167e15d
PeterEkwere <peterekwere04@gmail.com>
2023-07-02 03:26:03 -0700

    created a program for task 1

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+    This is a module that divides a matrix
+    author: peter ekwere
+
+"""
+
+
+def matrix_divided(matrix, div):
+    try:
+        row_sizes = len(matrix[0])
+        j = 1
+        new_matrix = []
+        for row in matrix:
+            if len(row) != row_sizes:
+                raise TypeError("Each row of the matrix "
+                                "must have the same size")
+            for item in row:
+                if not isinstance(item, (int, float)):
+                    raise TypeError("matrix must be a matrix (list of lists) "
+                                    "of integers/floats")
+
+        if not isinstance(div, (int, float)):
+            raise TypeError("div must be a number")
+        if div == 0:
+            raise ZeroDivisionError("division by zero")
+
+        for row in matrix:
+            new_row = []
+            for item in row:
+                number = item / div
+                rounded_num = round(number, 2)
+                new_row.append(rounded_num)
+            new_matrix.append(new_row)
+
+        return new_matrix
+    except (TypeError, ZeroDivisionError) as e:
+        return str(e)
