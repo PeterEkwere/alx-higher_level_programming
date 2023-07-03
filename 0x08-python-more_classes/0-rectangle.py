a2173e11f35ae45bf2b9d5d86f005f532206a445
PeterEkwere <peterekwere04@gmail.com>
2023-07-03 06:52:06 -0700

    created .py script for task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+    This is a Class module
+    Author: peter ekwere
+
+"""
+
+
+class Rectangle:
+    """
+    This is a Rectangle class
+    """
+    pass
