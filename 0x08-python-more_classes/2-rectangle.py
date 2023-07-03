afa2bef90ae146a6814bb9caf3a8a8d529ab1253
PeterEkwere <peterekwere04@gmail.com>
2023-07-03 14:04:54 -0700

    fixed indentation error for all tasks

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -12,21 +12,31 @@ class Rectangle:
     This is a Rectangle class
     """
     def __init__(self, width=0, height=0):
+        """
+        Initialize a new rectangle
+
+        width: this is an integer
+        height: this is an integer
+        """
         self.__width = width
         self.__height = height
 
     def area(self):
+        """ returns area of a rectangle """
         return self.__width * self.__height
 
     def perimeter(self):
+        """ returns the perimeter of a rectangle """
         return 2 * (self.__width + self.__height)
 
     @property
     def height(self):
+        """ getter for the attr height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ setter for the attr height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -35,10 +45,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """ getter for the attr width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ setter for the attr width """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -12,21 +12,31 @@ class Rectangle:
     This is a Rectangle class
     """
     def __init__(self, width=0, height=0):
+        """
+        Initialize a new rectangle
+
+        width: this is an integer
+        height: this is an integer
+        """
         self.__width = width
         self.__height = height
 
     def area(self):
+        """ returns the area of a rectangle """
         return self.__width * self.__height
 
     def perimeter(self):
+        """ returns the perimetr of a rectangle """
         return 2 * (self.__width + self.__height)
 
     @property
     def height(self):
+        """ getter of the attr height """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ setter of the attr height """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -35,10 +45,12 @@ class Rectangle:
 
     @property
     def width(self):
+        """ getter of the attr width """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ setter of the attr width """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -46,6 +58,7 @@ class Rectangle:
         self.__width = value
 
     def __str__(self):
+        """ __str__ is triggered when str or print is used """
         if 0 in [self.__width, self.__height]:
             return ""
         else:
