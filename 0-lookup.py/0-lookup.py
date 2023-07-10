2eb3bf5a88ebe2e750159bb9126ff63293cb4dec
PeterEkwere <peterekwere04@gmail.com>
2023-07-10 11:20:48 -0700

    initial commit for task 0

--- 0-lookup.py/0-lookup.py
+++ 0-lookup.py/0-lookup.py
@@ -5,4 +5,11 @@
     Author: Peter Ekwere
 
 """
+if __name__ == "__main__":
+    # This will execute if run directly
+    print("hello")
 
+
+def lookup(obj):
+    """ The lookup function returns a list of attr and meth of an object """
+    return list(dir(obj))
