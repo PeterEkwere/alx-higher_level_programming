92ba5052548752c5ebdcccf40102dd92aac66048
PeterEkwere <peterekwere04@gmail.com>
2023-07-10 15:22:05 -0700

    initial commit for task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+    This is a module
+    author: Peter Ekwere
+
+"""
+if __name__ == "__main__":
+    # This should not be run directly
+    print()
+
+
+def is_kind_of_class(obj, a_class):
+    if isinstance(obj, a_class) or issubclass(type(obj), a_class):
+        return True
