ead78dfbdb625c37c8dc6f60f3fe272388c81d15
PeterEkwere <peterekwere04@gmail.com>
2023-07-10 15:25:13 -0700

    added comment for function for task 3

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -11,5 +11,8 @@ if __name__ == "__main__":
 
 
 def is_kind_of_class(obj, a_class):
+    """
+    This is a class that checks is an object is an instance of a class
+    """
     if isinstance(obj, a_class) or issubclass(type(obj), a_class):
         return True
