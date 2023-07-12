1f625b97d0cf228032e69ddf9da2ff2f072c1dc2
PeterEkwere <peterekwere04@gmail.com>
2023-07-12 00:53:58 -0700

    added comments for function for task 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -11,6 +11,9 @@ if __name__ == "__main__":
 
 
 def append_write(filename="", text=""):
+    """
+    This is a function that appends to a file and returns the amount
+    """
     with open(filename, mode='a', encoding="utf-8") as a_file:
         num = a_file.write(text)
     return num
