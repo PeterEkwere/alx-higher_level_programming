733d55469d51c6c74b65ceafb4eabb613a687b6a
PeterEkwere <peterekwere04@gmail.com>
2023-07-12 00:40:34 -0700

    initial commit for task 1

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+    This is a Function Module
+    author: peter ekwere
+
+"""
+if __name__ == "__main__":
+    """ Do not run this directly """
+    print()
+
+
+def write_file(filename="", text=""):
+    with open(filename, mode='w', encoding='utf-8') as a_file:
+        Tfile = a_file.write(text)
+    return Tfile
