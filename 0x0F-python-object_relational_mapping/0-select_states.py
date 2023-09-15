9fbfa81ef31119ff9f8f6758a6ca82211c944843
PeterEkwere <peterekwere04@gmail.com>
2023-09-15 02:36:51 -0700

    initial commit for task 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+# -*- encoding: UTF-8 -*-
+"""
+    This Script would query the Database and print all states
+    Author: Peter Ekwere
+"""
+import sys
+import MySQLdb
+
+username = sys.argv[1]
+password = sys.argv[2]
+dbname = sys.argv[3]
+
+connection = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        password=password,
+        database=dbname,
+        charset="utf8"
+        )
+
+cursor = connection.cursor()
+query = "SELECT * FROM states ORDER BY id ASC"
+cursor.execute(query)
+query_rows = cursor.fetchall()
+for rows in query_rows:
+    print(rows)
+cursor.close()
+connection.close()
