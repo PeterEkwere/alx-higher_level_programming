a2b2ee390336163915d7b2f6cf27beccfe91f45e
PeterEkwere <peterekwere04@gmail.com>
2023-09-15 06:19:37 -0700

    initial commit for task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# -*- encoding: UTF-8 -*-
+"""
+    This Script would query the Database and print all states
+    Author: Peter Ekwere
+"""
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbname = sys.argv[3]
+    sort_by = sys.argv[4]
+
+    connection = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            password=password,
+            database=dbname,
+            charset="utf8"
+            )
+
+    cursor = connection.cursor()
+    query = "SELECT * FROM states WHERE name = '{}'"
+    formated_query = query.format(sort_by)
+    cursor.execute(formated_query)
+    query_rows = cursor.fetchall()
+    for rows in query_rows:
+        if rows[1] == sort_by:
+            print(rows)
+    cursor.close()
+    connection.close()
