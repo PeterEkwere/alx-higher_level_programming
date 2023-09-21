2f666e79b1a33758ba7321d2bf3e988509c03fa7
PeterEkwere <peterekwere04@gmail.com>
2023-09-21 13:13:00 -0700

    initial commit for task 7

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -3,28 +3,28 @@
 const PSquare = require('./5-square');
 
 class Square extends PSquare {
-	constructor(size) {
-		super(size, size);
-	}
+  constructor (size) {
+    super(size, size);
+  }
 
-	charPrint(c) {
-		if (c === 'C') {
-			for (let i = 0; i < 4; i++) {
-				let row = "";
-				for (let j = 0; j < 4; j++) {
-					row += "C"
-				}
-				console.log(row);
-			}
-		} else {
-			 for (let i = 0; i < 4; i++) {
-                                let row = "";
-                                for (let j = 0; j < 4; j++) {
-                                        row += "X"
-                                }
-                                console.log(row);
-                        }
-		}
-	}
+  charPrint (c) {
+    if (c === 'C') {
+      for (let i = 0; i < 4; i++) {
+        let row = '';
+        for (let j = 0; j < 4; j++) {
+          row += 'C';
+        }
+        console.log(row);
+      }
+    } else {
+      for (let i = 0; i < 4; i++) {
+        let row = '';
+        for (let j = 0; j < 4; j++) {
+          row += 'X';
+        }
+        console.log(row);
+      }
+    }
+  }
 }
 module.exports = Square;
--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,13 +1,13 @@
 #!/usr/bin/node
 
 exports.nbOccurences = function (list, searchElement) {
-	let count = 0;
-	let i = 0;
+  let count = 0;
+  let i = 0;
 
-	while(i < list.length) {
-		if (list[i] === searchElement) {
-			count++;
-		} i++;
-	}
-	return count;
-}
+  while (i < list.length) {
+    if (list[i] === searchElement) {
+      count++;
+    } i++;
+  }
+  return count;
+};
