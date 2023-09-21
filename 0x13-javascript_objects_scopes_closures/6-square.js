db97a6704b6faa89328565fd930a7ce53ee88e7e
PeterEkwere <peterekwere04@gmail.com>
2023-09-21 12:58:05 -0700

    initial commit for task 7

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+const PSquare = require('./5-square');
+
+class Square extends PSquare {
+	constructor(size) {
+		super(size, size);
+	}
+
+	charPrint(c) {
+		if (c === 'C') {
+			for (let i = 0; i < 4; i++) {
+				let row = "";
+				for (let j = 0; j < 4; j++) {
+					row += "C"
+				}
+				console.log(row);
+			}
+		} else {
+			 for (let i = 0; i < 4; i++) {
+                                let row = "";
+                                for (let j = 0; j < 4; j++) {
+                                        row += "X"
+                                }
+                                console.log(row);
+                        }
+		}
+	}
+}
+module.exports = Square;
