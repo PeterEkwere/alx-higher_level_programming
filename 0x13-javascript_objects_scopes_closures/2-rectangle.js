1e4fb1552747731344c7b1c0cc8555e0fa514b91
PeterEkwere <peterekwere04@gmail.com>
2023-09-20 23:30:19 -0700

    initial commit for task 1 2 3

---
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+class Rectangle {
+  constructor (w, h) {
+    if (w === 0 || h === 0 || w <= 0 || h <= 0) {
+      this.width = undefined;
+      this.height = undefined;
+    } else {
+      this.width = w;
+      this.height = h;
+    }
+  }
+}
+
+module.exports = Rectangle;
