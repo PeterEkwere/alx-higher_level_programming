56815951be4d9b744309fafc6552429bde1212dd
PeterEkwere <peterekwere04@gmail.com>
2023-09-29 05:37:08 -0700

    refactored task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,15 +6,12 @@
 """
 import urllib.request
 
-
-res = urllib.request.Request('https://alx-intranet.hbtn.io/status')
-with urllib.request.urlopen(res) as a:
-    html = a.read()
-html_string = html.decode()
-print(f"""Body response:
+if __name__ == "__main__":
+    res = urllib.request.Request('https://alx-intranet.hbtn.io/status')
+    with urllib.request.urlopen(res) as a:
+        html = a.read()
+        html_string = html.decode()
+        print(f"""Body response:
         - type: <class '{type(html).__name__}'>
         - content: {html}
         - utf8 content: {html_string}""")
-
-if __name__ == "__main__":
-    pass
