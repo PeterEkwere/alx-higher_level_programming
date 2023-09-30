b7c6e309c88aff669d73875e3c5d65365776d470
PeterEkwere <peterekwere04@gmail.com>
2023-09-29 17:25:16 -0700

    added condition to code

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -9,4 +9,7 @@ import sys
 if __name__ == "__main__":
     url = sys.argv[1]
     response = requests.get(url)
-    print("Error code: {}".format(response.status_code))
+    if response.status_code == 200:
+        print(response.text)
+    else:
+        print("Error code: {}".format(response.status_code))
